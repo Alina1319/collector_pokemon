d8e8938da555e7ec9fbaac25a64017281ea88c72
alina <alinaneculai19@gmail.com>
2021-06-02 11:08:16 +0100

    task1finished

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function getDB()
+{
+    $db = new PDO('mysql:host=db;dbname=pokemons','root','password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
+
+function getPokemons($db): array
+{
+    $query = $db->prepare('SELECT `name`, `pokemon_type`,`height`,`weight`,`ability` FROM `pokedex` JOIN `types` ON `pokedex`.`type` = `types`.`id`;');
+    $result = $query->execute();
+    $pokemon = $query->fetchALL();
+    return $pokemon;
+}
+
+function displayPokemon(array $pokemons): string
+{
+   $output = '<section class="pokedex">';
+   foreach ($pokemons as $pokemon){
+        $output .=  '<div class="description">'. '<div class="pokemon-name">'. '<p class="pokemon-heading">'. $pokemon['name']. '</p>' .'</div>'.
+
+                    '<div class="col">'. '<div class="type-class">'.'<p class="type">'.'Type: '.$pokemon['pokemon_type'] . '</p>'.'</div>'.
+
+                    '<div class="type-class">'.'<p class="height">'. 'Height: '.$pokemon['height']. '</p>'. '</div>' .'</div>'.
+
+                    '<div class="col">' . '<div class="type-class">' . '<p class="weight">' . 'Weight: '.$pokemon['weight'].'</p>'. '</div>'.
+
+                    '<div class="type-class">'. '<p class="ability">'.'Ability: '.$pokemon['ability']. '</p>'.'</div>'.'</div>'.'</div>';
+   }
+        $output .= '</section>';
+        return $output;
+}
+
---
+++ test/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+
+require '../functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+class functions extends TestCase
+{
+    public function testDisplayPokemonSucces()
+    {
+        $input = [
+            ['name'=>'bulbasaur',
+                'type'=>'grass',
+                'height'=>'2',
+                'weight'=>'2',
+                'ability'=>'overgrow'
+            ]
+        ];
+        $expected= '<section class="pokedex"><div class="description"><div class="pokemon-name"><p class="pokemon-heading">bulbasaur</p></div><div class="col"><div class="type-class"><p class="type">Type: </p></div>
+                    <div class="type-class"><p class="height">Height: 2</p></div></div><div class="col"><div class="type-class"><p class="weight">Weight: 2</p></div><div class="type-class"><p class="ability">Ability: overgrow</p></div>
+                    </div></div></section>';
+
+        $result = displayPokemon($input);
+        $this->assertEquals($expected, $result);
+    }
+    public function testDisplayPokemonFailure()
+    {
+        $input = [];
+        $result = DisplayPokemon($input);
+        $expected = '<section class="pokedex"></section>';
+        $this->assertEquals($expected, $result);
+    }
+    public function testDisplayPokemonMalformed()
+    {
+        $input = "";
+        $this->expectException(TypeError::class);
+        DisplayPokemon($input);
+    }
+}
