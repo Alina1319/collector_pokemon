55b308012959ee3fb798db26d4da63b68ed7dff5
alina <alinaneculai19@gmail.com>
2021-06-02 11:55:06 +0100

    testfilechanged

--- test/functions.php
+++ test/functions.php
@@ -18,7 +18,12 @@ class functions extends TestCase
                 'ability'=>'overgrow'
             ]
         ];
-        $expected= '<section class="pokedex"><div class="description"><div class="pokemon-name"><p class="pokemon-heading">bulbasaur</p></div><div class="col"><div class="type-class"><p class="type">Type: </p></div><div class="type-class"><p class="height">Height: 2</p></div></div><div class="col"><div class="type-class"><p class="weight">Weight: 2</p></div><div class="type-class"><p class="ability">Ability: overgrow</p></div></div></div></section>';
+        $expected = '<section class="pokedex"><div class="description"><div class="pokemon-name">' .
+                   '<p class="pokemon-heading">bulbasaur</p></div><div class="col"><div class="type-class">' .
+                   '<p class="type">Type: </p></div><div class="type-class"><p class="height">Height: 2</p>' .
+                    '</div></div><div class="col"><div class="type-class"><p class="weight">Weight: 2</p></div>' .
+                    '<div class="type-class">'.
+                   '<p class="ability">Ability: overgrow</p></div></div></div></section>';
         $result = displayPokemon($input);
         $this->assertEquals($expected, $result);
     }
