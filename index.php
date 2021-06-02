6d138c48568872058e5ed75e6750b5299158e487
alina <alinaneculai19@gmail.com>
2021-06-02 14:45:48 +0100

    maybe last changes

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+require "functions.php";
+
+$db = getDB();
+$pokemons = getPokemons($db);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Finger+Paint&family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
+    <link rel="icon" href="https://pngimg.com/uploads/pokeball/pokeball_PNG21.png">
+    <link rel="stylesheet" href="resources/css/normalize.css">
+
+</head>
+<body>
+<main>
+    <section class="pika-section">
+        <h1 class="pika-heading">Pokemon collection</h1>
+        <div class="pika">
+            <img class="main-img" src="./img/pika.png" alt="pika">
+        </div>
+    </section>
+<?php
+echo displayPokemon($pokemons);
+?>
+</main>
+</body>
+</html>
+
+
