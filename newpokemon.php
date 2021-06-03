97d9b9712a0fe61a98ef627ecabb7f86350f054b
alina <alinaneculai19@gmail.com>
2021-06-03 14:29:28 +0100

    story2finished

---
+++ newpokemon.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You!</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Finger+Paint&family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
+    <link rel="icon" href="https://pngimg.com/uploads/pokeball/pokeball_PNG21.png">
+</head>
+<body>
+<div class="main">
+    <div class="adding">
+        <p>Congratulation, you just caught a new pokemon. <br> Go and Catch 'Em All!
+        </p>
+    </div>
+    <div class="charmander">
+        <img src="./img/ash.png" alt="charmander">
+    </div>
+    <section class="btns">
+        <div class="more-pokemons">
+            <p class="add_button">Click here to go back to the pokedex</p>
+            <a href="./index.php" class="button  btn" style="vertical-align:middle"><span> Pokedex</span></a>
+        </div>
+        <div class="more-pokemons">
+            <p class="add_button">Click here to go back to the form</p>
+            <a href="./form.php" class="button  btn" style="vertical-align:middle"><span> Form</span></a>
+        </div>
+    </section>
+</div>
+</body>
+<?php
